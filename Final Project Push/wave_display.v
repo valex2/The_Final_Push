f030f44075502c57169f6df9d55a77f8fea60b71
valex2 <92407277+valex2@users.noreply.github.com>
2023-12-12 14:58:26 -0800

    most recent (still no song-reader 128 note)

--- Final Project Push/wave_display.v
+++ Final Project Push/wave_display.v
@@ -8,9 +8,9 @@ module wave_display (
     input read_index,
     output wire [8:0] read_address,
     output wire valid_pixel,
-    output reg [7:0] r,
-    output reg [7:0] g,
-    output reg [7:0] b
+    output wire [7:0] r,
+    output wire [7:0] g,
+    output wire [7:0] b
 );
 
 // Implement me! -- NO, begone!
@@ -67,52 +67,58 @@ wire valid_quadrant = ~((quadrant_pos == 2'b00) | (quadrant_pos == 2'b11) | (y[9
 
 assign valid_pixel = (valid & valid_quadrant & want_pixel & ({x[9], x[7:1]} > 8'd2) &({x[9], x[7:1]} < 8'd253)); // if the y_val is between the read values in the RAM, and we are in the right quadrant, and the right part of the screen, write to the display
 
-reg [29:0] next_color;
-wire [29:0] curr_color;
-dffr #(30) color (
-    .clk(clk),
-    .r(reset),
-    .d(next_color),
-    .q(curr_color)
-);
-// create an 8-bit counter to cycle 0-255 for ramp
-reg [7:0] reduced_count;
-always @(*) begin
-    case(curr_color)
-        22'bxxxxxxxxxxx1xxxxxxxxxx : reduced_count = reduced_count + 1;
-        default : reduced_count = reduced_count;
-        endcase
-end
+assign r = 8'd255;
+assign g = 8'd0;
+assign b = 8'd0;
 
-// implement HSV rainbow functionality on display lines
-always @(*) begin
-    if (curr_color >= 22'd0 && curr_color <= 22'd699050) begin
-        r = 8'd255;
-        g = reduced_count; //up
-        b = 8'd0;
-    end else if (curr_color >= 22'd699051 && curr_color <= 22'd1398101) begin
-        r = 8'd255 - reduced_count; // down
-        g = 8'd255;
-        b = 8'd0;
-    end else if (curr_color >= 22'd1348102 && curr_color <= 22'd2097152) begin
-        r = 8'd0;
-        g = 8'd255;
-        b = reduced_count; // up
-    end else if (curr_color >= 22'd2097153 && curr_color <= 22'd2796202) begin
-        r = 8'd0;
-        g = 8'd255 - reduced_count; // down
-        b = 8'd255;
-    end else if (curr_color >= 22'd2796203 && curr_color <= 22'd3495253) begin
-        r = reduced_count; // up
-        g = 8'd0;
-        b = 8'd255;
-    end else if (curr_color >= 22'd3495254 && curr_color <= 22'd4194303) begin
-        r = 8'd255;
-        g = 8'd0;
-        b = 8'd255 - reduced_count; // down
-    end
-    next_color = curr_color + 1;
-end
+
+
+//reg [29:0] next_color;
+//wire [29:0] curr_color;
+//dffr #(30) color (
+//    .clk(clk),
+//    .r(reset),
+//    .d(next_color),
+//    .q(curr_color)
+//);
+//// create an 8-bit counter to cycle 0-255 for ramp
+//reg [7:0] reduced_count;
+//always @(*) begin
+//    case(curr_color)
+//        22'bxxxxxxxxxxx1xxxxxxxxxx : reduced_count = reduced_count + 1;
+//        default : reduced_count = reduced_count;
+//        endcase
+//end
+
+//// implement HSV rainbow functionality on display lines
+//always @(*) begin
+//    if (curr_color >= 22'd0 && curr_color <= 22'd699050) begin
+//        r = 8'd255;
+//        g = reduced_count; //up
+//        b = 8'd0;
+//    end else if (curr_color >= 22'd699051 && curr_color <= 22'd1398101) begin
+//        r = 8'd255 - reduced_count; // down
+//        g = 8'd255;
+//        b = 8'd0;
+//    end else if (curr_color >= 22'd1348102 && curr_color <= 22'd2097152) begin
+//        r = 8'd0;
+//        g = 8'd255;
+//        b = reduced_count; // up
+//    end else if (curr_color >= 22'd2097153 && curr_color <= 22'd2796202) begin
+//        r = 8'd0;
+//        g = 8'd255 - reduced_count; // down
+//        b = 8'd255;
+//    end else if (curr_color >= 22'd2796203 && curr_color <= 22'd3495253) begin
+//        r = reduced_count; // up
+//        g = 8'd0;
+//        b = 8'd255;
+//    end else if (curr_color >= 22'd3495254 && curr_color <= 22'd4194303) begin
+//        r = 8'd255;
+//        g = 8'd0;
+//        b = 8'd255 - reduced_count; // down
+//    end
+//    next_color = curr_color + 1;
+//end
 
 /*
 always @(*) begin
